b7514ff2bd87925f0a560c962e72422e775594fb
arjunindia <arjun8107@gmail.com>
2021-07-13 18:48:50 +0530

    some stuff colors and stuff

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-player.autoplay("muted");
+function rndn() {
+  return Math.floor(Math.random() * 255);
+}
+
+setInterval(() => {
+  document.querySelector(
+    ".logo h2"
+  ).style.color = `rgb(${rndn()},${rndn()},${rndn()})`;
+}, 1000);
