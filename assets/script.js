6a32a74e64bbecd7457aea53ccb51ef467df20f5
arjunindia <arjun8107@gmail.com>
2021-07-14 16:05:22 +0530

    Added staffs section

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,35 @@ setInterval(() => {
     ".logo h2"
   ).style.color = `rgb(${rndn()},${rndn()},${rndn()})`;
 }, 1000);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("sdiv");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
