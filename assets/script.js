02d5fd17e620d31b271c7533fe1c48540b6b2f58
arjunindia <arjun8107@gmail.com>
2021-07-16 16:52:56 +0530

    Added Hamburger Menu

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 function rndn() {
   return Math.floor(Math.random() * 255);
 }
-
+let cc = 0;
 setInterval(() => {
   document.querySelector(
     ".logo h2"
@@ -38,4 +38,16 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
+}
+
+function hhc() {
+  if (cc % 2 == 0) {
+    document.querySelector(`.links-2`).classList.add(`hhc`);
+    document.querySelector(`.hh span`).innerHTML = `menu_open`;
+  } else {
+    document.querySelector(`.links-2`).classList.remove(`hhc`);
+    document.querySelector(`.hh span`).innerHTML = `menu`;
+  }
+  cc++;
+  console.log("opened");
 }
